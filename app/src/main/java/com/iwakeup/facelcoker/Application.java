df262269e18e744151c68b94e469942216109904
Ted <humin147@live.com>
2017-09-15 20:34:12 +0800

    Dialog

--- app/src/main/java/com/iwakeup/facelcoker/Application.java
+++ app/src/main/java/com/iwakeup/facelcoker/Application.java
@@ -14,6 +14,7 @@ public class Application extends android.app.Application {
 	private final String TAG = this.getClass().toString();
 	FaceDB mFaceDB;
 
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
